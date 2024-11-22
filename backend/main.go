9b37ce5ef2ad875a3a278f6c2179647c7f78601c
Jason Nie <jnie1@hawk.iit.edu>
2024-11-21 18:40:16 -0600

    writing dummy database access

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 // Handlers
@@ -37,15 +43,51 @@ func postHandler(c *gin.Context) {
 	})
 }
 
+func booksHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, databaseStuffGoesHere)
+}
+
 func main() {
 	// Initialize a Gin router
 	r := gin.Default()
 
+	// Loading environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Retrieving environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+
+	// Database connection string
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=%s", dbUser, dbPassword, dbName, dbHost, dbPort, dbSSLMode)
+
+	// Opening connection to database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error opening database: ", err)
+	}
+	defer db.Close()
+
+	// Pinging databse connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error pinging database: ", err)
+	}
+
 	// Define routes and associate them with handlers
 	r.GET("/greet", greetHandler)
 	r.GET("/user", userHandler)
+	r.GET("/books", booksHandler)
 	r.POST("/post", postHandler)
 
 	// Start the server on port 8080
 	r.Run(":8080")
+
 }
