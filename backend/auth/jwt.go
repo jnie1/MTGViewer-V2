c8c94886a8606509289aef091f597b35bb0912a4
billybimbob <noahluvsmilkduds@gmail.com>
2024-11-25 12:53:05 -0600

    add jwt and pword auth methods

---
+++ backend/auth/jwt.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"errors"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Claims struct {
+	jwt.StandardClaims
+	Role string `json:"role"`
+}
+
+var tokenKey = os.Getenv("TOKEN_KEY")
+
+func getTokenKey(_ *jwt.Token) (interface{}, error) {
+	return []byte(tokenKey), nil
+}
+
+func ParseToken(tokenString string) (claims *Claims, err error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, getTokenKey)
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+
+	if !ok {
+		return nil, errors.New("claims failed to parse")
+	}
+
+	return claims, nil
+}
+
+func GenerateToken(claims *Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenKey, err := getTokenKey(token)
+
+	if err != nil {
+		return "", err
+	}
+
+	return token.SignedString(tokenKey)
+}
