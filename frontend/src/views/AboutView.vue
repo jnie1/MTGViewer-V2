d4792f2f22b562e5b2bbe2bc2f18d1c1a071322c
billybimbob <noahluvsmilkduds@gmail.com>
2024-12-14 11:00:18 -0600

    prettier format

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+import fetchApi from '@/fetch/api';
+import { ref } from 'vue';
+
+interface ICard {
+  name: string;
+}
+
+const card = ref<ICard>();
+
+fetchApi<ICard>('cards/scryfall').then((c) => (card.value = c));
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page {{ card?.name }}</h1>
   </div>
 </template>
 
