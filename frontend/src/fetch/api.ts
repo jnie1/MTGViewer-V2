bca17530026b5ed1ab24bf9e7fa0e195335a0a50
billybimbob <noahluvsmilkduds@gmail.com>
2024-12-14 11:11:15 -0600

    include accept header

--- frontend/src/fetch/api.ts
+++ frontend/src/fetch/api.ts
@@ -3,19 +3,18 @@ import ResponseError from './ResponeError';
 const basePath = new URL(import.meta.env.VITE_API_URL);
 
 async function fetchApi<T = unknown>(path: string, init?: RequestInit): Promise<T> {
-    const fullPath = new URL(path, basePath);
-    const fullInit: RequestInit = {
-        ...init,
-        credentials: 'include',
-    };
+  const fullPath = new URL(path, basePath);
 
-    const response = await fetch(fullPath, fullInit);
-    if (!response.ok) {
-        throw new ResponseError(response);
-    }
+  const headers: HeadersInit = { ...init?.headers, ['Accept']: 'application/json' };
+  const fullInit: RequestInit = { ...init, headers, credentials: 'include' };
 
-    const body: T = await response.json();
-    return body;
+  const response = await fetch(fullPath, fullInit);
+  if (!response.ok) {
+    throw new ResponseError(response);
+  }
+
+  const body: T = await response.json();
+  return body;
 }
 
 export default fetchApi;
