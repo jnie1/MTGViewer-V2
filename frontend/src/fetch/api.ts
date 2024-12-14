30e2c2f7ee94f91cc511d3e6a38a5197f18a2d36
billybimbob <noahluvsmilkduds@gmail.com>
2024-12-14 11:00:46 -0600

    typo

--- frontend/src/fetch/api.ts
+++ frontend/src/fetch/api.ts
@@ -3,21 +3,19 @@ import ResponseError from './ResponeError';
 const basePath = new URL(import.meta.env.VITE_API_URL);
 
 async function fetchApi<T = unknown>(path: string, init?: RequestInit): Promise<T> {
-  const fullPath = new URL(path, basePath);
-  const fullInit: RequestInit = {
-    ...init,
-    credentials: 'include',
-  };
+    const fullPath = new URL(path, basePath);
+    const fullInit: RequestInit = {
+        ...init,
+        credentials: 'include',
+    };
 
-  const repsonse = await fetch(fullPath, fullInit);
+    const response = await fetch(fullPath, fullInit);
+    if (!response.ok) {
+        throw new ResponseError(response);
+    }
 
-  if (!repsonse.ok) {
-    throw new ResponseError(repsonse);
-  }
-
-  const body: T = await repsonse.json();
-
-  return body;
+    const body: T = await response.json();
+    return body;
 }
 
 export default fetchApi;
