30de634577593a618d81cd9ed71bcc69480e781e
billybimbob <noahluvsmilkduds@gmail.com>
2024-12-26 16:04:41 -0600

    use form component for sign up

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -2,11 +2,43 @@
 import fetchApi from '@/fetch/api';
 import { ref } from 'vue';
 
+const valid = ref(false);
 const name = ref('');
 const email = ref('');
 const password = ref('');
 
+const nameRules = [
+  (value: string | null) => {
+    if (value) return true;
+    return 'User Name is required.';
+  },
+];
+
+const emailRules = [
+  (value: string | null) => {
+    if (value) return true;
+    return 'Email is required.';
+  },
+  (value: string | null) => {
+    if (value && /.+@.+\..+/.test(value)) return true;
+    return 'Email must be valid.';
+  },
+];
+
+const passwordRules = [
+  (value: string | null) => {
+    if (value) return true;
+    return 'Password is required.';
+  },
+  (value: string) => {
+    if (value.length > 8) return true;
+    return 'Password must be at least 8 characters.';
+  },
+];
+
 const handleSubmit = async () => {
+  if (!valid.value) return;
+
   const signupRequest = {
     name: name.value,
     email: email.value,
@@ -15,27 +47,29 @@ const handleSubmit = async () => {
 
   await fetchApi('/signup', {
     method: 'POST',
+    body: JSON.stringify(signupRequest),
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(signupRequest),
   });
 };
 </script>
 
 <template>
   <main>
-    <form @submit.prevent="handleSubmit">
-      <label for="user-name">Name</label>
-      <input id="user-name" v-model="name" required />
-
-      <label for="email">Email</label>
-      <input id="email" v-model="email" required type="email" />
-
-      <label for="password">Password</label>
-      <input id="password" v-model="password" required type="password" />
-
-      <button type="submit">Sign up</button>
-    </form>
+    <v-sheet class="mx-auto" width="300">
+      <v-form v-model="valid" validate-on="submit" fail-fast @submit.prevent="handleSubmit">
+        <v-text-field label="User Name" v-model="name" required :rules="nameRules" />
+        <v-text-field label="Email" v-model="email" required type="email" :rules="emailRules" />
+        <v-text-field
+          label="Password"
+          v-model="password"
+          required
+          type="password"
+          :rules="passwordRules"
+        />
+        <v-btn class="ma-2 mt-0" type="submit">Sign up</v-btn>
+      </v-form>
+    </v-sheet>
   </main>
 </template>
