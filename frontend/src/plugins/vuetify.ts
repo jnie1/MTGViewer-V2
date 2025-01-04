ebbd5b7e9393bc097142d1b94c7c8533f37fb697
billybimbob <noahluvsmilkduds@gmail.com>
2025-01-03 18:27:15 -0600

    vuetify variants

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -6,13 +6,16 @@ const customTheme: ThemeDefinition = {
   dark: true,
   colors: {
     background: '#111519',
+    'background-variant': '#212935',
     'on-background': '#e0eaea',
     'on-background-variant': '#f8f8f8',
     surface: '#232e3f',
     'on-surface': '#e0eaea',
     'on-surface-variant': '#f8f8f8',
     primary: '#fa4125',
+    'primary-variant': '#fdc200',
     secondary: '#7aa5f7',
+    'secondary-variant': '#02446e',
   },
 };
 
