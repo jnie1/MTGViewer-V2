0e0255a80161313d6a425950eaa2e8cfa88a9655
Xionghui Huang <xionghuih745@gmail.com>
2025-01-23 23:23:23 -0600

    created nav bar for each page

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {RouterLink} from 'vue-router';
+</script>
+
+<template>
+  <header id="NavBar">
+    <nav>
+      <ul>
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+        <li><RouterLink to="/card">Card</RouterLink></li>
+        <li><RouterLink to="/signup">SignUp</RouterLink></li>
+      </ul> 
+    </nav>
+  </header>
+</template>
+
+
+<style scoped>
+  nav {
+    color:antiquewhite;
+    position: relative;
+  }
+
+  ul, li {
+    list-style-type: none;
+    display: inline;
+    padding: 1%;
+    font-size: 15px;
+  }
+</style>
