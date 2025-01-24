9eba90932f241d69335201c5b5571bbc88b1e595
Xionghui Huang <xionghuih745@gmail.com>
2025-01-23 21:44:00 -0600

    created a nav component for homepage

---
+++ frontend/src/components/HomePageHeading.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <nav>
+      <href to="/">Home</href>
+      <href to="/about">About</href>
+      <href to="/card">Card</href>
+      <href to="/signup">SignUp</href>
+    </nav>
+    <h1>Hello World</h1>
+  </header>
+</template>
+
