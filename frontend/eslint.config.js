80e93c133833f1066c1ecdcd2e433560d7fc5593
billybimbob <noahluvsmilkduds@gmail.com>
2026-02-27 03:24:19 -0600

    update eslint rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'eslint/config';
 import pluginVue from 'eslint-plugin-vue';
-import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import pluginVitest from '@vitest/eslint-plugin';
 import prettierConfig from '@vue/eslint-config-prettier';
 
-export default defineConfig([
+export default defineConfigWithVueTs([
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
@@ -15,8 +14,14 @@ export default defineConfig([
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
+  ...pluginVue.configs['flat/recommended-error'],
+  {
+    rules: {
+      'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+      'vue/require-default-prop': ['off'],
+    },
+  },
+  vueTsConfigs.strict,
 
   {
     ...pluginVitest.configs.recommended,
