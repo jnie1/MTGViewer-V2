e619dc2f03d20e56fb485c88caa7767ce3da4439
Jason Nie <jnie1@hawk.iit.edu>
2026-02-28 16:23:11 -0600

    made production opt in, moved similar code to different file.

--- backend/dev.go
+++ backend/dev.go
@@ -1,15 +1,10 @@
-//go:build dev
+//go:build !prod
 
 package main
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"github.com/jnie1/MTGViewer-V2/auth"
-	"github.com/jnie1/MTGViewer-V2/database"
-	"github.com/jnie1/MTGViewer-V2/routes"
 	"github.com/joho/godotenv"
 )
 
@@ -19,28 +14,5 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	if err := database.Open(); err != nil {
-		log.Fatal("Error opening database: ", err)
-	}
-
-	defer database.Close()
-
-	// Initialize a Gin router
-	r := gin.Default()
-	r.Use(auth.AddCors)
-
-	// Define routes and associate them with handlers
-	r.GET("/greet", greetHandler)
-	routes.AddUserRoutes(r)
-	routes.AddBookRoutes(r)
-	routes.AddCardRoutes(r)
-
-	authorized := r.Group("", auth.IsAuthorized)
-
-	authorized.GET("/secret", func(c *gin.Context) {
-		c.JSON(http.StatusAccepted, gin.H{"secret": "some secret"})
-	})
-
-	// Start the server on port 8080
-	r.Run(":8080")
+	RegisterRouter()
 }
--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,11 @@
-//go:build !dev
+//go:build prod
 
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jnie1/MTGViewer-V2/auth"
-	"github.com/jnie1/MTGViewer-V2/database"
-	"github.com/jnie1/MTGViewer-V2/routes"
 )
 
 // Handlers
@@ -20,29 +16,5 @@ func greetHandler(c *gin.Context) {
 }
 
 func main() {
-
-	if err := database.Open(); err != nil {
-		log.Fatal("Error opening database: ", err)
-	}
-
-	defer database.Close()
-
-	// Initialize a Gin router
-	r := gin.Default()
-	r.Use(auth.AddCors)
-
-	// Define routes and associate them with handlers
-	r.GET("/greet", greetHandler)
-	routes.AddUserRoutes(r)
-	routes.AddBookRoutes(r)
-	routes.AddCardRoutes(r)
-
-	authorized := r.Group("", auth.IsAuthorized)
-
-	authorized.GET("/secret", func(c *gin.Context) {
-		c.JSON(http.StatusAccepted, gin.H{"secret": "some secret"})
-	})
-
-	// Start the server on port 8080
-	r.Run(":8080")
+	RegisterRouter()
 }
