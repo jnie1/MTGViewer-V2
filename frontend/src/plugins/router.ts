9cebe5f75e93534e1b91b51c7ee6caa5a051a75d
Xionghui Huang <xionghuih745@gmail.com>
2026-03-02 08:39:35 -0600

    fixed the view name

--- frontend/src/plugins/router.ts
+++ frontend/src/plugins/router.ts
@@ -3,7 +3,7 @@ import HomeView from '@/views/HomeView.vue';
 import SignupView from '@/views/SignupView.vue';
 import CardView from '@/views/CardView.vue';
 import ChangeView from '@/views/ChangeView.vue';
-import TransactionDetail from '@/views/Transaction.vue';
+import TransactionView from '@/views/TransactionView.vue';
 import ContainerDetail from '@/views/Container.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -39,7 +39,7 @@ const router = createRouter({
     {
       path: '/transaction/:groupId',
       name: 'TransactionDetail',
-      component: TransactionDetail,
+      component: TransactionView,
     },
     {
       path: '/containers/:containerId',
