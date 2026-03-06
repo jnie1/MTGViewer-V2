4f137f37a0b41a17c78f09691326da696ab9715a
billybimbob <noahluvsmilkduds@gmail.com>
2026-03-06 01:22:05 -0600

    tweak style of card image

--- frontend/src/cards/CardImage.vue
+++ frontend/src/cards/CardImage.vue
@@ -2,36 +2,40 @@
 import type { ICard } from '@/cards/types';
 
 interface ICardImageProps {
-  card?: ICard;
+  card: ICard;
+  highlight?: boolean;
 }
 
-const { card } = defineProps<ICardImageProps>();
+const { card, highlight } = defineProps<ICardImageProps>();
 </script>
 
 <template>
   <v-img
     class="card-img"
     :class="{
-      uncommon: card?.rarity === 'uncommon',
-      rare: card?.rarity === 'rare',
-      mythic: card?.rarity === 'mythic',
+      shadow: highlight,
+      uncommon: card.rarity === 'uncommon',
+      rare: card.rarity === 'rare',
+      mythic: card.rarity === 'mythic',
     }"
-    :alt="card?.name"
-    :src="card?.imageUrls.normal"
-    :lazy-src="card?.imageUrls.preview"
+    :alt="card.name"
+    :src="card.imageUrls.normal"
+    :lazy-src="card.imageUrls.preview"
   />
 </template>
 
 <style lang="css" scoped>
 .card-img {
-  width: 300px;
-  max-width: 300px;
+  height: 468px;
+  width: 336px;
   border-radius: 16px;
 }
 
 @media (min-width: 768px) {
   .card-img {
-    transition: all 200ms ease-in;
+    transition:
+      transform 200ms ease-in,
+      box-shadow 200ms ease-in;
   }
 
   .card-img:hover {
@@ -39,15 +43,15 @@ const { card } = defineProps<ICardImageProps>();
     transform: scale(1.05);
   }
 
-  .card-img.uncommon:hover {
+  .card-img.shadow.uncommon:hover {
     box-shadow: var(--shadow-length) var(--color-secondary);
   }
 
-  .card-img.rare:hover {
+  .card-img.shadow.rare:hover {
     box-shadow: var(--shadow-length) var(--color-primary-variant);
   }
 
-  .card-img.mythic:hover {
+  .card-img.shadow.mythic:hover {
     box-shadow: var(--shadow-length) var(--color-primary);
   }
 }
