3b909ca307bd49dc05876a4c64ec44f2d15099f9
blueshysky <xionghuih745@gmail.com>
2026-03-07 02:34:39 -0600

    add starter logic for merging logs

---
+++ backend/transactions/merge.go
@@ -0,0 +1,50 @@
+package transactions
+
+import "github.com/google/uuid"
+
+func MergeLogs(logs []TransactionLogs) []TransactionLogs {
+	additions := map[uuid.UUID][]*TransactionLogs{}
+	removals := map[uuid.UUID][]*TransactionLogs{}
+
+	for _, log := range logs {
+		if log.FromContainer != nil {
+			cardId := logs.ScryfallId
+			removals[cardId] = append(removals[cardId], &log)
+		}
+		if log.ToContainer != nil {
+			cardId := logs.ScryfallId
+			additions[cardId] = append(additions[cardId], &log)
+		}
+	}
+
+	logUpdates := map[int]int{}
+    for cardId, adds := range additions {
+        removes := removals[cardId]
+        i, j := 0, 0
+        for i < len(adds) && j < len(removes) {
+            // TODO add some log updates based on joins
+        }
+    }
+
+	if len(logUpdates) == 0 {
+		return logs
+	}
+
+	updatedLogs := make([]TransactionLogs, len(logs))
+	for _, log := range logs {
+		updatedAmount := log.Quantity + logUpdates[log.TransactionId]
+		if updatedAmount < 0 {
+			continue
+		}
+		updatedLogs = append(updatedLogs, TransactionLogs{
+			logs.TransactionId,
+			logs.GroupId,
+			logs.FromContainer,
+			logs.ToContainer,
+			logs.ScryfallId,
+			updatedAmount,
+		})
+	}
+
+	return updatedLogs
+}
