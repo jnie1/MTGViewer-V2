037e44d3674c4df3c14a91d2c43690df5df598ee
billybimbob <noahluvsmilkduds@gmail.com>
2026-03-08 16:20:29 -0500

    some progress

--- backend/transactions/merge.go
+++ backend/transactions/merge.go
@@ -2,7 +2,78 @@ package transactions
 
 import "github.com/google/uuid"
 
+type changeKey struct {
+	scryfallId  uuid.UUID
+	containerId int
+}
+
+type transactionDelta struct {
+	transactionId int
+	delta         int
+}
+
 func MergeLogs(logs []TransactionLogs) []TransactionLogs {
+	changes := map[changeKey]transactionDelta{}
+
+	for _, log := range logs {
+		if log.FromContainer != nil {
+			key := changeKey{log.ScryfallId, log.FromContainer.ContainerId}
+
+			if delta, ok := changes[key]; ok {
+				changes[key] = transactionDelta{
+					transactionId: delta.transactionId,
+					delta:         delta.delta + log.Quantity,
+				}
+			} else {
+				changes[key] = transactionDelta{
+					transactionId: log.TransactionId,
+					delta:         log.Quantity,
+				}
+			}
+		}
+		if log.ToContainer != nil {
+			key := changeKey{log.ScryfallId, log.ToContainer.ContainerId}
+
+			if delta, ok := changes[key]; ok {
+				changes[key] = transactionDelta{
+					transactionId: delta.transactionId,
+					delta:         delta.delta - log.Quantity,
+				}
+			} else {
+				changes[key] = transactionDelta{
+					transactionId: log.TransactionId,
+					delta:         -log.Quantity,
+				}
+			}
+		}
+	}
+
+	logsById := make(map[int]*TransactionLogs, len(logs))
+	for _, log := range logs {
+		logsById[log.TransactionId] = &log
+	}
+
+	updatedLogs := make([]TransactionLogs, len(changes))
+	for _, delta := range changes {
+		log, ok := logsById[delta.transactionId]
+		if !ok {
+			return logs
+		}
+		if log == nil {
+			return logs
+		}
+
+		var newLog TransactionLogs
+		if delta.delta > 0 {
+			newLog = TransactionLogs{}
+		} else {
+			newLog = TransactionLogs{
+				TransactionId: delta.transactionId,
+			}
+		}
+
+	}
+
 	additions := map[uuid.UUID][]*TransactionLogs{}
 	removals := map[uuid.UUID][]*TransactionLogs{}
 
@@ -22,6 +93,23 @@ func MergeLogs(logs []TransactionLogs) []TransactionLogs {
 		removes := removals[cardId]
 		i, j := 0, 0
 		for i < len(adds) && j < len(removes) {
+			add := adds[i]
+			if add == nil {
+				i += 1
+				continue
+			}
+
+			remove := removes[j]
+			if remove == nil {
+				j += 1
+				continue
+			}
+
+			if add.Quantity < remove.Quantity {
+				logUpdates[add.TransactionId] = logUpdates[add.TransactionId]
+			} else if add.Quantity > remove.Quantity {
+			} else {
+			}
 			// TODO add some log updates based on joins
 		}
 	}
