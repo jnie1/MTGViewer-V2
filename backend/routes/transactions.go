5c95e9fec5492f1a04405c33d7d646b36bfc481e
billybimbob <noahluvsmilkduds@gmail.com>
2026-03-09 18:22:04 -0500

    add endpoint for sync up

--- backend/routes/transactions.go
+++ backend/routes/transactions.go
@@ -51,8 +51,54 @@ func fetchTransactionLogs(c *gin.Context) {
 	c.JSON(http.StatusOK, listOfReportCards)
 }
 
+func fetchTransactionRange(c *gin.Context) {
+	g1 := c.Param("group1")
+	g2 := c.Param("group2")
+
+	group1, err := uuid.Parse(g1)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	group2, err := uuid.Parse(g2)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	logRange, err := transactions.FetchLogRange(group1, group2)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	listOfLogs, err := transactions.FetchLogsFromRange(logRange)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	scryfallIds := transactions.GetScryfallIds(listOfLogs)
+	cards, err := cards.FetchCollection(scryfallIds)
+
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	listOfReportCards, err := transactions.JoinReportCards(cards, listOfLogs)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, listOfReportCards)
+}
+
 func AddTransactionRoutes(router *gin.Engine) {
 	group := router.Group("/logs")
 	group.GET("", fetchUpdateLogs)
 	group.GET("/:group", fetchTransactionLogs)
+	group.GET("/:group1/:group2", fetchTransactionRange)
 }
