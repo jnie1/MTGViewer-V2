c9d15b50db285ed979773d3d8a865914334fb6ad
Xionghui Huang <xionghuih745@gmail.com>
2026-07-27 18:02:23 -0500

    removed container id and added capacity

--- frontend/src/collection/CollectionContainer.vue
+++ frontend/src/collection/CollectionContainer.vue
@@ -13,21 +13,16 @@ const sortedContainers = containers.sort((a, b) => a.containerId - b.containerId
 <template>
   <div v-if="sortedContainers && sortedContainers.length > 0">
     <v-row class="table-header">
-      <v-col>Container ID</v-col>
       <v-col>Container Name</v-col>
     </v-row>
     <v-divider />
     <v-row v-for="(container, index) in sortedContainers" :key="index" class="table">
       <v-col>
         <router-link :to="{ name: 'container', params: { containerId: container.containerId } }">
-          {{ container.containerId }}
-        </router-link>
-      </v-col>
-      <v-col>
-        <router-link :to="{ name: 'container', params: { containerId: container.containerId } }">
           {{ container.name }}
         </router-link>
       </v-col>
+      <v-col>{{ container.capacity }}</v-col>
     </v-row>
   </div>
 </template>
