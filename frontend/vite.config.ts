4325bda02f97f49b1c1a66f3c180a01cbec75613
billybimbob <noahluvsmilkduds@gmail.com>
2026-07-27 21:00:38 -0500

    try out without the whole url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://jnie1.github.io/MTGViewer-V2/',
+  base: '/MTGViewer-V2/',
   plugins: [vue(), vueJsx(), vueDevTools(), vuetify()],
   resolve: {
     alias: {
