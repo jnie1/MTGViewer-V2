ed0dd5f1266fe12149c55691665ab4603d4b8d06
Jason <jason.nie.feng@gmail.com>
2026-07-29 16:21:51 -0500

    updated CORS logic to properly implement Client Origins.

--- backend/auth/middleware.go
+++ backend/auth/middleware.go
@@ -3,14 +3,39 @@ package auth
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
+var allowedOrigins []string
+
+// Function runs one time upon initialization, before main() gets called
+func init() {
+	origins := os.Getenv("CLIENT_ORIGINS")
+
+	//Parse the CLIENT_ORIGINS string into multiple tokens and append it to the list of valid Origins
+	for _, origin := range strings.Split(origins, ",") {
+		trimmedOrigin := strings.TrimSpace(origin)
+		if trimmedOrigin != "" {
+			allowedOrigins = append(allowedOrigins, trimmedOrigin)
+		}
+	}
+}
+
 func AddCors(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", os.Getenv("CLIENT_ORIGIN"))
+	requestOrigin := c.Request.Header.Get("Origin")
+
+	for _, origin := range allowedOrigins {
+		if origin == requestOrigin {
+			c.Header("Access-Control-Allow-Origin", requestOrigin)
+			break
+		}
+	}
+
 	c.Header("Access-Control-Allow-Credentials", "true")
 	c.Header("Access-Control-Allow-Headers", "Content-Type,Authorization")
+	c.Header("Vary", "Origin")
 
 	if c.Request.Method == "OPTIONS" {
 		c.AbortWithStatus(http.StatusNoContent)
