b526f0cc2656c216b7cd02ee4f81bdfe60463ace
billybimbob <noahluvsmilkduds@gmail.com>
2026-08-01 00:33:37 -0500

    add catch all route

--- backend/routes/static.go
+++ backend/routes/static.go
@@ -4,42 +4,52 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func AddStaticPaths(r gin.IRouter) error {
+func AddStaticPaths(r *gin.Engine) error {
 	cwd, err := os.Getwd()
 	if err != nil {
 		return err
 	}
 
-	routeMap := map[string]string{
-		"/":       path.Join(cwd, "dist/index.html"),
-		"/assets": path.Join(cwd, "dist/assets"),
+	indexPath := path.Join(cwd, "dist", "index.html")
+	assetsPath := path.Join(cwd, "dist", "assets")
+
+	_, err = os.Stat(indexPath)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
 	}
 
-	for route, filepath := range routeMap {
-		stat, err := os.Stat(filepath)
-		if os.IsNotExist(err) {
-			continue
-		}
-		if err != nil {
-			return err
-		}
-		if stat.IsDir() {
-			r.Static(route, filepath)
-		} else {
-			r.StaticFile(route, filepath)
-		}
+	_, err = os.Stat(assetsPath)
+	if os.IsNotExist(err) {
+		return nil
 	}
+	if err != nil {
+		return err
+	}
+
+	r.Static("/", indexPath)
+	r.StaticFile("/assets", assetsPath)
 
 	r.GET("/index", func(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "/")
 	})
-
 	r.GET("/index.html", func(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		if strings.HasPrefix(c.Request.URL.Path, "/api/") {
+			c.Status(http.StatusNotFound)
+			return
+		}
+		c.File(indexPath)
 	})
 
 	return nil
