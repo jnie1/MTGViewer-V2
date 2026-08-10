8f542c1c088a46999c50139a3d27071fd4272af3
billybimbob <noahluvsmilkduds@gmail.com>
2026-08-10 11:42:17 -0500

    skip the intermediate type

--- backend/cards/mtgjson.go
+++ backend/cards/mtgjson.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/google/uuid"
 	mtgjson "github.com/mtgjson/mtgjson-sdk-go"
@@ -13,29 +12,6 @@ import (
 
 var sdk *mtgjson.SDK
 var ErrMissingCards = errors.New("missing cards")
-
-type mtgJsonCard struct {
-	MtgJsonId       uuid.UUID `json:"uuid"`
-	ScryfallId      uuid.UUID `json:"scryfallId"`
-	ManaCost        string    `json:"manaCost,omitempty"`
-	Name            string    `json:"name"`
-	SetName         string    `json:"setName"`
-	Set             string    `json:"setCode"`
-	CollectorNumber string    `json:"number"`
-	MultiverseId    string    `json:"multiverseId,omitempty"`
-	Power           string    `json:"power,omitempty"`
-	Toughness       string    `json:"toughness,omitempty"`
-	Type            string    `json:"type"`
-	Rarity          string    `json:"rarity"`
-}
-
-type mtgJsonId struct {
-	ScryfallId      uuid.UUID `json:"scryfallId"`
-	Name            string    `json:"name"`
-	Set             string    `json:"setCode"`
-	CollectorNumber string    `json:"number"`
-	MultiverseId    string    `json:"multiverseId,omitempty"`
-}
 
 func OpenSDK() (*mtgjson.SDK, error) {
 	var err error
@@ -56,10 +32,10 @@ func FetchRandomCard(ctx context.Context) (Card, error) {
 		"ci.scryfallId",
 		"c.manaCost",
 		"c.name",
-		"s.name AS setName",
+		"s.name AS set",
 		"c.setCode",
-		"c.number",
-		"ci.multiverseId",
+		"c.number AS collectorNumber",
+		"CAST(ci.multiverseId AS INTEGER) AS multiverseId",
 		"c.power",
 		"c.toughness",
 		"c.type",
@@ -68,7 +44,7 @@ func FetchRandomCard(ctx context.Context) (Card, error) {
 
 	query, params := sql.Build()
 	query += " USING SAMPLE 1"
-	var results []mtgJsonCard
+	var results []Card
 
 	if err := sdk.Connection().ExecuteInto(ctx, &results, query, params...); err != nil {
 		return Card{}, err
@@ -78,7 +54,14 @@ func FetchRandomCard(ctx context.Context) (Card, error) {
 		return Card{}, ErrMissingCards
 	}
 
-	return fromJsonCard(results[0])
+	result := results[0]
+	images, err := ImageURLs(result.ScryfallId)
+	if err != nil {
+		return Card{}, err
+	}
+
+	result.Images = images
+	return result, nil
 }
 
 func FetchCard(ctx context.Context, scryfallId uuid.UUID) (Card, error) {
@@ -91,14 +74,13 @@ func FetchCard(ctx context.Context, scryfallId uuid.UUID) (Card, error) {
 	sql.Join("JOIN sets AS s ON s.code = c.setCode")
 	sql.WhereEq("ci.scryfallId", scryfallId)
 	sql.Select(
-		"c.uuid",
 		"ci.scryfallId",
 		"c.manaCost",
 		"c.name",
-		"s.name AS setName",
+		"s.name AS set",
 		"c.setCode",
-		"c.number",
-		"ci.multiverseId",
+		"c.number AS collectorNumber",
+		"CAST(ci.multiverseId AS INTEGER) AS multiverseId",
 		"c.power",
 		"c.toughness",
 		"c.type",
@@ -107,17 +89,24 @@ func FetchCard(ctx context.Context, scryfallId uuid.UUID) (Card, error) {
 	sql.Limit(1)
 
 	query, params := sql.Build()
-	var matches []mtgJsonCard
+	var results []Card
 
-	if err := sdk.Connection().ExecuteInto(ctx, &matches, query, params...); err != nil {
+	if err := sdk.Connection().ExecuteInto(ctx, &results, query, params...); err != nil {
 		return Card{}, err
 	}
 
-	if len(matches) == 0 {
+	if len(results) == 0 {
 		return Card{}, fmt.Errorf("no matching card found for %s", scryfallId)
 	}
 
-	return fromJsonCard(matches[0])
+	result := results[0]
+	images, err := ImageURLs(result.ScryfallId)
+	if err != nil {
+		return Card{}, err
+	}
+
+	result.Images = images
+	return result, nil
 }
 
 func FetchCollection(ctx context.Context, scryfallIds uuid.UUIDs) ([]Card, error) {
@@ -132,8 +121,8 @@ func FetchCollection(ctx context.Context, scryfallIds uuid.UUIDs) ([]Card, error
 	sql := db.NewSQLBuilder("cards AS c")
 	sql.Join("JOIN card_identifiers AS ci ON ci.uuid = c.uuid")
 	sql.Join("JOIN sets AS s ON s.code = c.setCode")
-	// TODO: maybe chunking?
 
+	// TODO: maybe chunking?
 	vals := make([]any, len(scryfallIds))
 	for i, id := range scryfallIds {
 		vals[i] = id
@@ -141,14 +130,13 @@ func FetchCollection(ctx context.Context, scryfallIds uuid.UUIDs) ([]Card, error
 	sql.WhereIn("ci.scryfallId", vals)
 
 	sql.Select(
-		"c.uuid",
 		"ci.scryfallId",
 		"c.manaCost",
 		"c.name",
-		"s.name AS setName",
+		"s.name AS set",
 		"c.setCode",
-		"c.number",
-		"ci.multiverseId",
+		"c.number AS collectorNumber",
+		"CAST(ci.multiverseId AS INTEGER) AS multiverseId",
 		"c.power",
 		"c.toughness",
 		"c.type",
@@ -156,20 +144,19 @@ func FetchCollection(ctx context.Context, scryfallIds uuid.UUIDs) ([]Card, error
 	)
 
 	query, params := sql.Build()
-	var matches []mtgJsonCard
+	var results []Card
 
-	if err := sdk.Connection().ExecuteInto(ctx, &matches, query, params...); err != nil {
+	if err := sdk.Connection().ExecuteInto(ctx, &results, query, params...); err != nil {
 		return nil, err
 	}
 
-	results := make([]Card, len(matches))
-
-	for i, s := range matches {
-		id, err := fromJsonCard(s)
+	for i := range results {
+		result := &results[i]
+		images, err := ImageURLs(result.ScryfallId)
 		if err != nil {
 			return nil, err
 		}
-		results[i] = id
+		result.Images = images
 	}
 
 	return results, nil
@@ -190,83 +177,18 @@ func FetchIdentifiers(ctx context.Context, ids CardIdQuery) ([]CardId, error) {
 	WhereIds(sql, ids, "ci.multiverseId", "c.name", "c.setCode", "c.number")
 	sql.Select(
 		"ci.scryfallId",
-		"ci.multiverseId",
 		"c.name",
 		"c.setCode",
-		"c.number",
+		"c.number AS collectorNumber",
+		"CAST(ci.multiverseId AS INTEGER) AS multiverseId",
 	)
 
 	query, params := sql.Build()
-	var matches []mtgJsonId
+	var results []CardId
 
-	if err := sdk.Connection().ExecuteInto(ctx, &matches, query, params...); err != nil {
+	if err := sdk.Connection().ExecuteInto(ctx, &results, query, params...); err != nil {
 		return nil, err
 	}
 
-	results := make([]CardId, len(matches))
-
-	for i, m := range matches {
-		id, err := fromJsonId(m)
-		if err != nil {
-			return nil, err
-		}
-		results[i] = id
-	}
-
 	return results, nil
-}
-
-func fromJsonCard(source mtgJsonCard) (Card, error) {
-	multiverseId, err := parseMultiverseId(source.MultiverseId)
-	if err != nil {
-		return Card{}, err
-	}
-
-	images, err := ImageURLs(source.ScryfallId)
-	if err != nil {
-		return Card{}, err
-	}
-
-	card := Card{
-		source.ScryfallId,
-		source.Name,
-		source.ManaCost,
-		source.SetName,
-		source.Set,
-		source.CollectorNumber,
-		multiverseId,
-		source.Type,
-		source.Rarity,
-		source.Power,
-		source.Toughness,
-		images,
-		map[string]string{},
-	}
-
-	return card, nil
-}
-
-func fromJsonId(source mtgJsonId) (CardId, error) {
-	multiverseId, err := parseMultiverseId(source.MultiverseId)
-	if err != nil {
-		return CardId{}, err
-	}
-
-	card := CardId{
-		source.ScryfallId,
-		source.Name,
-		source.Set,
-		source.CollectorNumber,
-		multiverseId,
-	}
-
-	return card, nil
-}
-
-func parseMultiverseId(source string) (int, error) {
-	if source != "" {
-		return strconv.Atoi(source)
-	} else {
-		return 0, nil
-	}
 }
