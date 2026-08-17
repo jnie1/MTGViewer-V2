56305501919b7595c2e2c5274b3dc73a41b94923
billybimbob <noahluvsmilkduds@gmail.com>
2026-08-17 13:50:47 -0500

    standardize json image proerty name

--- backend/cards/types.go
+++ backend/cards/types.go
@@ -49,7 +49,7 @@ type CardImagePreview struct {
 
 type CardImageAmount struct {
 	ScryfallId uuid.UUID     `json:"scryfallId"`
-	Images     CardImageURLs `json:"images"`
+	Images     CardImageURLs `json:"imagesUrls"`
 	Amount     int           `json:"amount"`
 }
 
--- frontend/src/container/types.ts
+++ frontend/src/container/types.ts
@@ -2,19 +2,17 @@ import type { ICard } from '@/cards/types';
 
 export interface IPrints {
   scryfallId: string;
+  imagesUrls: ICard['imageUrls'];
   amount: number;
-  images: {
-    preview?: string;
-    normal?: string;
-    full?: string;
-  };
 }
+
 export interface ICardContainer {
   containerId: string;
   name: string;
   amount: number;
   prints: IPrints[];
 }
+
 export interface ICardContainerMatch {
   card: ICard;
   containers: ICardContainer[];
