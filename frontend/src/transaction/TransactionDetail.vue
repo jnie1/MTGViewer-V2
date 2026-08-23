227d57f91f707a70fb9c30dd0d238ee15a3a0dbb
blueshysky <xionghuih745@gmail.com>
2026-08-23 14:10:20 -0500

    testing

--- frontend/src/transaction/TransactionDetail.vue
+++ frontend/src/transaction/TransactionDetail.vue
@@ -34,6 +34,7 @@ const { changes } = defineProps<ITransactionProps>();
               params: { scryfallId: change.card.scryfallId },
             }"
           >
+            asdasd
             {{ change.card.name }}
           </router-link>
         </v-col>
