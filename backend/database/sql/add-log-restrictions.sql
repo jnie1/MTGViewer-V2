2efb4fb17afea9d99f720a38a10181224ff8da3d
billybimbob <noahluvsmilkduds@gmail.com>
2026-08-28 00:51:46 -0500

    rename sql file

--- backend/database/sql/add-log-restrictions.sql
+++ backend/database/sql/add-log-restrictions.sql
@@ -2,12 +2,10 @@
 
 ALTER TABLE transactions 
     ADD CONSTRAINT unique_exchange UNIQUE (group_id, from_container_id, to_container_id, scryfall_id),
-    ADD CONSTRAINT positive_amount CHECK (amount > 0),
-    ADD COLUMN description TEXT;
+    ADD CONSTRAINT positive_amount CHECK (amount > 0);
 
 -- rollback
 
 ALTER TABLE transactions 
     DROP CONSTRAINT unique_exchange,
-    DROP CONSTRAINT positive_amount,
-    DROP COLUMN description;
+    DROP CONSTRAINT positive_amount;
