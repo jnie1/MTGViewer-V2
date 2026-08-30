7b6da932935fd8871343c1d44dca648deb169ddd
billybimbob <noahluvsmilkduds@gmail.com>
2026-08-30 16:33:15 -0500

    update login/signup components

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import fetchApi from '@/fetch/api';
+import fetchApi, { type AccessTokenInfo, setAccessToken } from '@/fetch/api';
 import ResponseError from '@/fetch/ResponseError';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
@@ -10,20 +10,6 @@ const valid = ref(false);
 const username = ref('');
 const password = ref('');
 const errorMessage = ref('');
-
-const usernameRules = [
-  (value: string | null) => {
-    if (value) return true;
-    return 'Username is required.';
-  },
-];
-
-const passwordRules = [
-  (value: string | null) => {
-    if (value) return true;
-    return 'Password is required.';
-  },
-];
 
 const handleSubmit = async () => {
   if (!valid.value) return;
@@ -36,14 +22,16 @@ const handleSubmit = async () => {
   };
 
   try {
-    await fetchApi('/login', {
+    const info = await fetchApi<AccessTokenInfo>('/login', {
       method: 'POST',
+      credentials: 'omit',
       body: JSON.stringify(loginRequest),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
+    setAccessToken(info);
     router.push('/');
   } catch (e) {
     if (e instanceof ResponseError && e.status === 400) {
@@ -58,13 +46,34 @@ const handleSubmit = async () => {
 <template>
   <main>
     <v-sheet class="mx-auto" width="300">
-      <v-form v-model="valid" validate-on="submit" fail-fast @submit.prevent="handleSubmit">
-        <v-text-field v-model="username" label="Username" required :rules="usernameRules" />
-        <v-text-field v-model="password" label="Password" required type="password" :rules="passwordRules"/>
+      <v-form v-model="valid" fail-fast @submit.prevent="handleSubmit">
+        <v-text-field
+          v-model="username"
+          label="Username"
+          required
+          :rules="[
+            (value: string | null) => {
+              if (value) return true;
+              return 'Required';
+            },
+          ]"
+        />
+        <v-text-field
+          v-model="password"
+          label="Password"
+          required
+          type="password"
+          :rules="[
+            (value: string | null) => {
+              if (value) return true;
+              return 'Required';
+            },
+          ]"
+        />
         <v-alert v-if="errorMessage" type="error" density="compact" class="mb-2">
           {{ errorMessage }}
         </v-alert>
-        <v-btn class="ma-2 mt-0" color="primary" type="submit">Log in</v-btn>
+        <v-btn class="ma-2" color="primary" type="submit">Log in</v-btn>
       </v-form>
     </v-sheet>
   </main>
--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,28 +1,13 @@
 <script setup lang="ts">
-import fetchApi from '@/fetch/api';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import fetchApi from '@/fetch/api';
+
+const router = useRouter();
 
 const valid = ref(false);
 const username = ref('');
 const password = ref('');
-
-const usernameRules = [
-  (value: string | null) => {
-    if (value) return true;
-    return 'Username is required.';
-  },
-];
-
-const passwordRules = [
-  (value: string | null) => {
-    if (value) return true;
-    return 'Password is required.';
-  },
-  (value: string) => {
-    if (value.length > 8) return true;
-    return 'Password must be at least 8 characters.';
-  },
-];
 
 const handleSubmit = async () => {
   if (!valid.value) return;
@@ -34,21 +19,49 @@ const handleSubmit = async () => {
 
   await fetchApi('/signup', {
     method: 'POST',
+    credentials: 'omit',
     body: JSON.stringify(signupRequest),
     headers: {
       'Content-Type': 'application/json',
     },
   });
+
+  router.push('/login');
 };
 </script>
 
 <template>
   <main>
     <v-sheet class="mx-auto" width="300">
-      <v-form v-model="valid" validate-on="submit" fail-fast @submit.prevent="handleSubmit">
-        <v-text-field v-model="username" label="Username" required :rules="usernameRules"/>
-        <v-text-field v-model="password" label="Password" required type="password" :rules="passwordRules"/>
-        <v-btn class="ma-2 mt-0" color="primary" type="submit">Sign up</v-btn>
+      <v-form v-model="valid" fail-fast @submit.prevent="handleSubmit">
+        <v-text-field
+          v-model="username"
+          label="Username"
+          required
+          :rules="[
+            (value: string | null) => {
+              if (value?.trim()) return true;
+              return 'Required';
+            },
+          ]"
+        />
+        <v-text-field
+          v-model="password"
+          label="Password"
+          required
+          type="password"
+          :rules="[
+            (value: string | null) => {
+              if (value) return true;
+              return 'Required';
+            },
+            (value: string) => {
+              if (value.length > 8) return true;
+              return 'Must be at least 8 characters';
+            },
+          ]"
+        />
+        <v-btn class="ma-2" color="primary" type="submit">Sign up</v-btn>
       </v-form>
     </v-sheet>
   </main>
