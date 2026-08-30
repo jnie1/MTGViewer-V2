9e4e57b9d8a00df38484e1c62f0ab18cd93f4412
billybimbob <noahluvsmilkduds@gmail.com>
2026-08-30 16:31:32 -0500

    drill down jwt from local storage to auth header

--- frontend/src/fetch/api.ts
+++ frontend/src/fetch/api.ts
@@ -1,13 +1,44 @@
 import ResponseError from './ResponseError';
 
+export interface AccessTokenInfo {
+  token: string;
+  expires: number;
+}
+
+export function setAccessToken(info: AccessTokenInfo) {
+  const item = JSON.stringify(info);
+  localStorage.setItem('accessToken', item);
+}
+
+export function isExpired(info: AccessTokenInfo): boolean {
+  const expires = info.expires * 1_000; // secs -> ms
+  const now = Date.now();
+  return expires <= now;
+}
+
 async function fetchApi<T = unknown>(path: string, init?: RequestInit): Promise<T> {
   const fullPath = new URL(path, import.meta.env.VITE_API_URL);
   fullPath.pathname = '/api' + fullPath.pathname;
 
-  const headers: HeadersInit = { ...init?.headers, ['Accept']: 'application/json' };
-  const fullInit: RequestInit = { ...init, headers, credentials: 'include' };
+  const headers = new Headers(init?.headers);
+  headers.set('Accept', 'application/json');
 
-  const response = await fetch(fullPath, fullInit);
+  if (init?.credentials !== 'omit') {
+    const item = localStorage.getItem('accessToken');
+    if (item) {
+      const info: AccessTokenInfo = JSON.parse(item);
+      if (info.token && !isExpired(info)) {
+        headers.set('Authorization', `Bearer ${info.token}`);
+      }
+    }
+  }
+
+  const response = await fetch(fullPath, {
+    ...init,
+    headers,
+    credentials: 'omit',
+  });
+
   if (!response.ok) {
     throw new ResponseError(response);
   }
