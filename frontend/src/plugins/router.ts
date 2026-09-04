b54ad15b5147a83c172a6a909a6eb4eef7a0044a
billybimbob <noahluvsmilkduds@gmail.com>
2026-09-03 21:19:25 -0500

    formatting

--- frontend/src/plugins/router.ts
+++ frontend/src/plugins/router.ts
@@ -90,9 +90,11 @@ const router = createRouter({
   ],
 });
 
-
 router.beforeEach((to) => {
-  if ((to.meta.requiresAdmin && !isAdmin.value) || (to.meta.requiresLoggedIn && !isLoggedIn.value)) {
+  if (
+    (to.meta.requiresAdmin && !isAdmin.value) ||
+    (to.meta.requiresLoggedIn && !isLoggedIn.value)
+  ) {
     return { name: 'home' };
   }
 });
