5e0ca0d9a88dcdba35781d1234aa815ecd0e0296
Xionghui Huang <xionghuih745@gmail.com>
2026-09-05 20:52:53 -0500

    added custom vite settings to disable ms

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,15 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), vuetify()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    vuetify({
+      styles: {
+        configFile: 'src/styles/settings.scss',
+      },
+    })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
